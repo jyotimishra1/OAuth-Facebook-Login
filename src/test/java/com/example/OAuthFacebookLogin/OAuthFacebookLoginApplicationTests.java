b771f3852b62efbc40a121c9dc7addcb81aaf676
jyotimishra1 <jyotimishra.jec@gmail.com>
2020-07-25 23:42:00 +0530

    Initial commit

---
+++ src/test/java/com/example/OAuthFacebookLogin/OAuthFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthFacebookLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
